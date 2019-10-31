0788a1d309cf4b7146907b350b454ccae08998b5
Emily Little <emlittle4@gmail.com>
2019-10-31 13:06:12 -0400

    s

--- android/src/main/java/com/gettipsi/stripe/GoogleApiPayFlowImpl.java
+++ android/src/main/java/com/gettipsi/stripe/GoogleApiPayFlowImpl.java
@@ -256,6 +256,7 @@ public final class GoogleApiPayFlowImpl extends PayFlow {
             // Generally, there is no need to show an error to
             // the user as the Google Pay API will do that.
             payPromise.reject(
+              status.getStatusMessage(),
               status.getStatusMessage()
             );
             break;
